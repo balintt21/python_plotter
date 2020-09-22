ed2f72611f61b2e6489cdd88a4685bcffa59b0f2
Balint Telkes <balintt21@gmail.com>
2020-09-22 10:30:36 +0200

    New plotter version
    Depens on matplotlib only

--- plotter.py
+++ plotter.py
@@ -1,25 +1,33 @@
 #!/usr/bin/env python3
 
+import os
 import sys
-import pandas as pd
-import plotly.express as px
+import json
+import errno
+import matplotlib.pyplot as plt
+
 
 def main():
-    if len(sys.argv) > 1:
-        try:
-            df = pd.read_csv(sys.argv[1])
-            y_array=list(df['y'].to_numpy())
-            max_y=max(y_array)
-            avg_y=sum(y_array)/len(y_array)
-            min_y=min(y_array)
-            plot_title=f'Min: {min_y} Avg: {avg_y} Max: {max_y}'
-            if (len(sys.argv) > 2) and (sys.argv[2] == "--bar"):
-                fig = px.bar(df, x='x', y='y', title=plot_title)
-            else:
-                fig = px.line(df, x='x', y='y', title=plot_title)
-            fig.show()
-        except Exception as e:
-            print(f"error: {str(e)}")
+    if len(sys.argv) < 2 or ("-h" in sys.argv) or ("--help" in sys.argv):
+        print(f"Usage: {sys.argv[0]} <json_file_path|text:json|text:python array>")
+        print("Input:\n\ttext:json\n\t\t{ \"x\": [1,2,3,...], \"y\": [1,2,3,...] }\n\ttext:python array\n\t\t[1,2,3,...]\n")
+        exit(0)
+    
+    data = []
+    arg = sys.argv[1]
+    if os.path.isfile(arg):
+        with open(arg, "r") as file:
+            data = json.load(file)
     else:
-        print(f"Usage: {sys.argv[0]} <data_path.csv> [OPTIONS]\nOptions:\n\t--line - Shows line chart (default)\n\t      OR\n\t--bar  - Shows bar chart\n\nData format: x,y\\n<[x value,y value\\n]*\nExample:\n\tx,y\n\t1,2\n\t3,4\n\t5,6\n ")
-main()
+        data = json.loads(arg)
+    
+    if isinstance(data, list):
+        plt.plot(data)
+    elif isinstance(data,dict) and ("x" in data) and ("y" in data):
+        plt.plot(data["x"], data["y"])
+    else:
+        exit(errno.EINVAL)
+    plt.show()
+
+if __name__ == "__main__":
+    main()
